8f8123f7c9b7ea393c7670d6eafa5706f1b07ef0
phoenix <vova24848@gmail.com>
2024-03-30 15:26:33 +0300

    complete UserRepository

--- src/server/domain/repository/ChatRepository.ts
+++ src/server/domain/repository/ChatRepository.ts
@@ -3,14 +3,4 @@ import {Chat} from "../entity/Chat";
 import {db} from "./Pool";
 
 export class ChatRepository implements BaseRepository<Chat> {
-    async save(entity: Chat) {
-        const data = await db.one('INSERT INTO users(name, active) VALUES($1, $2) RETURNING id', ['John', true])
-        entity.id = data.id
-        return entity
-    }
-    update: (entity: Chat) => Promise<Chat>;
-    remove: (entity: Chat) => Promise<Chat>;
-    findById: (id: number) => Promise<Chat>;
-    findByIds: (ids: number[]) => Promise<Chat[]>;
-    findByField: (name: string, value: FindValue) => Promise<Chat[]>;
 }
