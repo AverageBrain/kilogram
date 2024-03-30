31efb88820504aa2044f2eac8fa9a7d82e3c4129
AverageBrain <anton101003@bk.ru>
2024-03-30 12:51:03 +0300

    add users

---
+++ src/mock/users.js
@@ -0,0 +1,32 @@
+export const users = [
+  {
+    id: '1',
+    createdAt: '2024-03-30T12:38:31',
+    name: 'Anton',
+    username: 'average-brain', 
+  }, 
+  {
+    id: '2',
+    createdAt: '2024-03-30T12:38:31',
+    name: 'Polina',
+    username: 'buchurella', 
+  }, 
+  {
+    id: '3',
+    createdAt: '2024-03-30T12:38:31',
+    name: 'Vova',
+    username: 'phoenixNazarov', 
+  }, 
+  {
+    id: '4',
+    createdAt: '2024-03-30T12:38:31',
+    name: 'Vlad',
+    username: 'SvVlad', 
+  }, 
+  {
+    id: '5',
+    createdAt: '2024-03-30T12:38:31',
+    name: 'Yuliana',
+    username: 'Yuliana427', 
+  }, 
+]
