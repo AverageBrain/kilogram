60f25e6f337ce0b009933f484d727c2de43912ec
AverageBrain <anton101003@bk.ru>
2024-04-01 19:34:49 +0300

    fix name SendForm.css

--- src/client/components/chatPage/SendForm/SendForm.tsx
+++ src/client/components/chatPage/SendForm/SendForm.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Form, Input, Button } from 'antd';
 import { SendOutlined } from '@ant-design/icons';
-import './index.css'
+import './SendForm.css'
 
 const SendMessage: React.FC = () => {
   return (
