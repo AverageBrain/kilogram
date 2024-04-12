2bb6874cb3c30dbeb1c4e7977dd5844b18e94b55
phoenix <vova24848@gmail.com>
2024-04-12 11:03:33 +0300

    Исправил экран входа при перезагрузки страницы

--- src/client/stores/AuthUserStore.ts
+++ src/client/stores/AuthUserStore.ts
@@ -14,6 +14,7 @@ class AuthUserStore extends BaseStore<UserType> {
 
             loadSelectedItem: action.bound,
         });
+        this.loading = true
     }
 
     async loadSelectedItem(): Promise<void> {
