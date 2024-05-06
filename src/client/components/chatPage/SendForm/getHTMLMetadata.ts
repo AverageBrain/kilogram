96c61813a67e62d831a3eab71ce1a5e1a98c7ed5
Germanova Polina <buchurella2003@gmail.com>
2024-05-06 20:41:21 +0300

    Добавлена метаинформация для ссылок

---
+++ src/client/components/chatPage/SendForm/getHTMLMetadata.ts
@@ -0,0 +1,34 @@
+import { MetadataType } from "../../../../types";
+
+const extractFirstLink = (htmlString: string): string | null => {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(htmlString, 'text/html');
+  const links = doc.getElementsByTagName('a');
+
+  if (links.length > 0) {
+    return links[0].href;
+  } else {
+    return null;
+  }
+}
+
+export const getHTMLMetadata = async (
+  htmlString: string,
+  getMetadata: (url: string) => Promise<void | MetadataType>,
+) => {
+  const firstLink = extractFirstLink(htmlString);
+  if (firstLink) {
+    const data = await getMetadata(firstLink);
+
+    if (data) {
+      return `<div class="metadata"><span class="metadata-title"><a href="${firstLink}">${data.title}</a></span><span class="metadata-description">${data?.description ?? ''}</span>
+          ${data?.imageUrl && !data.imageUrl.includes('static')
+            ? `<img style="height: 200px;width: 200px" alt="${data.title}" src="${data?.imageUrl}">`
+            : ''}
+        </div>
+      `;
+    }
+  }
+
+  return '';
+};
